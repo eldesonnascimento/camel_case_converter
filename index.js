25820a33e280dd27a7f2a9f0ef98c192e5aea032
eldesonnascimento <122459383+eldesonnascimento@users.noreply.github.com>
2023-09-08 15:17:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-let symbols = ["-","_"]
-let str= document.querySelector(".myInput").value;
-function toCamelCase(str){
+let symbols = ["-", "_"];
 
-const haveSymbol = symbols.some(symbol =>str.includes(symbol))
+function toCamelCase(str) {
+  const haveSymbol = symbols.some(symbol => str.includes(symbol));
 
-if(haveSymbol){
-   str= str.replace(/[-_][a-z]/g, match => match.toUpperCase())
-  str = str.replace(/-|_/g,"")
-  return str
-}else{
-  return str
+  if (haveSymbol) {
+    str = str.replace(/[-_][a-z]/g, match => match.toUpperCase());
+    str = str.replace(/-|_/g, "");
+    return str;
+  } else {
+    return str;
+  }
 }
-}
 
-
+const button = document.querySelector(".button");
 
 button.addEventListener('click', function() {
   const inputElement = document.querySelector(".myInput");
   const inputValue = inputElement.value;
+
+  // Verifique se o campo de entrada não está vazio antes de prosseguir
+  if (inputValue.trim() === "") {
+    alert("Por favor, insira uma palavra antes de clicar em 'Enviar'.");
+    return;
+  }
+
   const convertedValue = toCamelCase(inputValue);
 
   // Crie um novo elemento <p>
@@ -29,9 +35,9 @@ button.addEventListener('click', function() {
   // Adicione o elemento <p> como filho do inputElement
   inputElement.parentNode.appendChild(pElement);
 
-  // Limpe o campo de entrada e o elemento <p> após 10segundos (10 000 milissegundos)
+  // Limpe o campo de entrada e o elemento <p> após 10 segundos (10 000 milissegundos)
   setTimeout(function() {
     inputElement.value = "";
     pElement.remove(); // Remove o elemento <p>
-  }, 10000); // 1000 milissegundos = 1 segundo.
+  }, 10000); // 10000 milissegundos = 10 segundos.
 });
